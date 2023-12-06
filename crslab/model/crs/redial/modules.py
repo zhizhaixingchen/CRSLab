7193d579554f40b995440818b6bed48b408ea76e
zhizhaixingchen <51323935+zhizhaixingchen@users.noreply.github.com>
2023-12-06 10:07:55 +0800

    Update modules.py
    pack_padded_sequence需要cpu适配

--- crslab/model/crs/redial/modules.py
+++ crslab/model/crs/redial/modules.py
@@ -71,7 +71,7 @@ class HRNN(nn.Module):
         if self.use_dropout:
             embedded = self.dropout(embedded)
 
-        packed_utterances = pack_padded_sequence(embedded, sorted_lengths, batch_first=True)
+        packed_utterances = pack_padded_sequence(embedded, sorted_lengths.to("cpu"), batch_first=True)
         _, utterance_encoding = self.utterance_encoder(packed_utterances)
 
         # concat the hidden states of the last layer (two directions of the GRU)
@@ -104,7 +104,7 @@ class HRNN(nn.Module):
 
         # reorder in decreasing sequence length
         sorted_representations = utterance_encoding.index_select(0, sorted_idx)
-        packed_sequences = pack_padded_sequence(sorted_representations, sorted_lengths, batch_first=True)
+        packed_sequences = pack_padded_sequence(sorted_representations, sorted_lengths.to("cpu"), batch_first=True)
 
         _, context_state = self.dialog_encoder(packed_sequences)
         context_state = context_state.index_select(1, rev_idx)
@@ -144,7 +144,7 @@ class SwitchingDecoder(nn.Module):
         sorted_lengths, sorted_idx, rev_idx = sort_for_packed_sequence(request_lengths)
         sorted_request = request.index_select(0, sorted_idx)
         embedded_request = self.embedding(sorted_request)  # (batch_size, max_utterance_length, embed_dim)
-        packed_request = pack_padded_sequence(embedded_request, sorted_lengths, batch_first=True)
+        packed_request = pack_padded_sequence(embedded_request, sorted_lengths.to("cpu"), batch_first=True)
 
         sorted_context_state = context_state.index_select(0, sorted_idx)
         h_0 = sorted_context_state.unsqueeze(0).expand(
